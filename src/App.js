259a341bf59d5987e88d0bc2d21d3d86da44dfdf
kiran97171 <120737308+kiran97171@users.noreply.github.com>
2023-04-12 22:56:47 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import HOC from './Components/HOC';
+import AddBackgroundColor from './Components/AddBackgroundColor';
+import ChangeColor from './Components/ChangeColor';
+import PureComp from './Components/PureComp';
+import NormalComp from './Components/NormalComp';
+
+function App() {
+  return (
+    <>
+      <div><h1>HOC : </h1></div>
+      <AddBackgroundColor value={<HOC/>}/>
+      <ChangeColor value={<HOC/>}/>
+      <div><h1>Normal Component : </h1></div>
+      <NormalComp/>
+      <div><h1>Pure Component : </h1></div>
+      <PureComp/>
+    </>
+  );
+}
+export default App;
